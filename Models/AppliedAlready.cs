ddad3595690689f3b79e6b90118d8a3e4c62bc84
Hijaker <30287680+Hijaker@users.noreply.github.com>
2024-12-16 14:27:02 +0100

    AppliedAlready class updated

--- Models/AppliedAlready.cs
+++ Models/AppliedAlready.cs
@@ -4,10 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MiniProj.Functionality;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
 
 namespace MiniProj.Models
 {
     public class AppliedAlready
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Org Number")]
+        public string CompanyNo { get; set; }
+
+        [BsonElement("X")]
+        public string? TagX { get; set; } = null;
+
+        [BsonElement("Descriptor")]
+        public string? Descriptor { get; set; } = null;
     }
 }
